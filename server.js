0f0080cbd30b9ebfa6e7c9f25819e2f50252054c
Takahiro Hara <takahiro.hb@gmail.com>
2021-01-27 01:16:15 +0900

    add router

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 // httpモジュールの要求。
 // Node.jsに同梱されているhttpモジュールへアクセスできるようになる。
 const http = require("http");
+const url = require("url");
 
-function start() {
+function start(route) {
   function onRequest(request, response) {
+    const pathname = url.parse(request.url).pathname;
     console.log("Request received.");
+
+    route(pathname);
+
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
